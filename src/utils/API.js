18c8a4da3ec476cfa6d46836ba2245988e486fde
James <J.Lemons2@gmail.com>
2018-10-23 11:38:18 -0400

    update

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,8 @@
-// import from'dotenv'.config()
 import axios from "axios";
-const BASEURL = "https://www.omdbapi.com/?t=";
+const BASEURL = "https://www.omdbapi.com/?s=";
 const APIKEY = process.env.REACT_APP_API_KEY;
+//Just in case zip file  did not produce .env file. You can unccomment line 5.
+// const APIKEY = "&apikey=9f572b90";  
 
 export default {
   search: function(query) {
